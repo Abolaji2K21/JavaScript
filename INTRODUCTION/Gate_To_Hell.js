f59c9629aa1d4b9903b3613b81761b854d7e6e4f
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-20 16:30:13 +0100

    update

--- INTRODUCTION/Gate_To_Hell.js
+++ INTRODUCTION/Gate_To_Hell.js
@@ -1,8 +1,11 @@
 var var1 = 2000;
 var var2 = var1;
 var var3 = var2;
-console.log(var3);
+console.log(var3);g
  
 
 console.log(var1 + var2 + var3); 
 console.log("HELLO TO MY WORLD");
+
+
+console.log(var1 * var2 * var3);
