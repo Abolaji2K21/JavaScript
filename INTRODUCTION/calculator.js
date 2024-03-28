46322b1b5dad86f3c56cb2beaf741cf532c5ba6a
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-28 14:35:24 +0100

    Perfect

--- INTRODUCTION/calculator.js
+++ INTRODUCTION/calculator.js
@@ -1,20 +1,34 @@
 var left_operand = 15;
 var right_operand = 10;
-var operator = "";
+var operator = "+";
 
 
-if(operator == "-"){
+if(operator === "-"){
     console.log(left_operand - right_operand)
-} else if (operator == "+"){
+} else if (operator === "+"){
     console.log(left_operand + right_operand)
 
-} else if (operator == "*"){
+} else if (operator === "*"){
     console.log(left_operand * right_operand)
 
-}else if (operator == "/"){
+}else if (operator === "/"){
     console.log(left_operand / right_operand)
  
 } else {
     console.log(NaN)
+
+}
+
+switch(operator){
+    case "*" :     console.log(left_operand * right_operand)
+        break
+    case "+" :     console.log(left_operand + right_operand)
+        break
+    case "-" :     console.log(left_operand - right_operand)
+        break
+    case "/" :     console.log(left_operand / right_operand)
+        break
+
+    default:      console.log(NaN)
 
 }
