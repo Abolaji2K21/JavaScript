7b26ca3ecde3d440c2a0d8063a738ab26bad7073
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-28 14:10:52 +0100

    JS

--- INTRODUCTION/Example.js
+++ INTRODUCTION/Example.js
@@ -2,12 +2,52 @@
 // console.log(name)
 
 
-function factorial(n){
-    if (n == 0){
-        return 1;
-    } else {
-        return factorial(n -1) * n;
-    }
+// function factorial(n){
+//     if (n == 0){
+//         return 1;
+//     } else {
+//         return factorial(n -1) * n;
+//     }
+// }
+
+// console.log(factorial(8));
+
+
+// let person = {
+//     firstname: "John",
+//     lastname: "Doe",
+//     age: 30,
+//     gender: "male"
+// }
+
+// console.log(person)
+
+
+// let array = [];
+// // let arr = new Array(3);
+// array[0] = `Hello`
+// array[1] = 1
+// array[2] = 4
+// array[3] = 6
+// array[4] = 2
+// array[5] = 9
+// console.log(array)
+
+
+
+let person = {
+    id: 25,
+    available: true,
+    count: 10,
+    firstname: "John",
+    lastname: "Doe",
+    author: "Mr Chi",
+    gender: "male"
 }
 
-console.log(factorial(8));
+console.log(person)
+
+
+let number = 5;
+let result =(number > 2);
+console.log(result);
