4924169c881a59cf911893fafbc24cc9fe5a6f0f
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-03-28 14:19:47 +0100

    calculator

---
+++ INTRODUCTION/calculator.js
@@ -0,0 +1,20 @@
+var left_operand = 15;
+var right_operand = 10;
+var operator = "";
+
+
+if(operator == "-"){
+    console.log(left_operand - right_operand)
+} else if (operator == "+"){
+    console.log(left_operand + right_operand)
+
+} else if (operator == "*"){
+    console.log(left_operand * right_operand)
+
+}else if (operator == "/"){
+    console.log(left_operand / right_operand)
+ 
+} else {
+    console.log(NaN)
+
+}
