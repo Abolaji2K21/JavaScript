aae6e77a783dd491d050631d6239eab47d74e6d6
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-04-17 14:58:59 +0100

    Perfect

--- INTRODUCTION/array.js
+++ INTRODUCTION/array.js
@@ -1,28 +1,57 @@
 let number = [1,2,3,4,5,6,7]
 
-// number.pop()
-// number.push(8)
+// // number.pop()
+// // number.push(8)
 
-// console.log(number)
+// // console.log(number)
 
-// number.shift()
-// console.log(number)
-// number.unshift(18)
-// console.log(number)
-
-
-// console.log(number.slice(1,3,2))
-
-// console.log(number.splice(1,3))
-
-// number.splice(2,3,2,3,0)
-// console.log(number)
+// // number.shift()
+// // console.log(number)
+// // number.unshift(18)
+// // console.log(number)
 
 
+// // console.log(number.slice(1,3,2))
+
+// // console.log(number.splice(1,3))
+
+// // number.splice(2,3,2,3,0)
+// // console.log(number)
 
 
-let num = [5,10,15,20]
-let result = number.concat(num)
+// let num = [5,10,15,20]
+// let result = number.concat(num)
 
-console.log(number.concat(num))
-console.log(result)
+// console.log(number.concat(num))
+// console.log(result)
+
+let display = [["car",   2000,], ["Truck",   500, ],["Bike",   6500,]]
+// console.log(display)
+// console.log(display[0][0])
+// console.log(display[1])
+// console.log(display[2])
+
+// let newDisplay = [["Scotter",1500]]
+// let result = display.concat(newDisplay)
+
+// console.log(result)
+
+display[0].push(["Toyota", "Nissan"])
+display[1].push(["Ford"])
+display[2].push(["Honda"])
+
+console.log(display)
+
+
+let example = [[200,50,300,5],[10,25,7,100],[25,20,70,45],[500,175,11,35]]
+
+console.log([example[1][0],example[1][1],example[1][2]])
+
+example[1].pop()
+console.log(example[1])
+
+example[0].shift
+console.log(example[1])
+
+example[1].slice()
+console.log(example[1])
