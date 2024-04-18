05998f775113ee29d680af5305cfaa63333450e5
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-04-18 11:36:14 +0100

    Test_pass

---
+++ INTRODUCTION/jestTest.test.js
@@ -0,0 +1,14 @@
+const addNumbers = require("./add.js");
+
+// let addNumbers = require("./add.js")
+
+
+
+test("Add two numbers ", () =>{
+    let a = 3 
+    let b = 5 
+
+    let answer = addNumbers(a,b)
+
+    expect(answer).toBe(8)
+})
