2c93397285fd11830fd8ab02e0dd47c6cab93d94
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-04-18 11:00:41 +0100

    okay

---
+++ INTRODUCTION/add.js
@@ -0,0 +1,8 @@
+function addNumbers(x,y){
+    let answer = x + y 
+    return answer
+}
+
+addNumbers(5,7)
+
+module.exports = addNumbers;
--- INTRODUCTION/forLoop.js
+++ INTRODUCTION/forLoop.js
@@ -1,134 +1,136 @@
-// let user = {
+// // let user = {
 
-//     firstName: "Dayo",
-//     lastName: "Miriam",
-//     age:       "100",
-//     sex:        "Female"
+// //     firstName: "Dayo",
+// //     lastName: "Miriam",
+// //     age:       "100",
+// //     sex:        "Female"
 
 
+// // }
+
+// // for(const key in user){
+// //     console.log(user[key])
+// // }
+
+// // for(const key of user){
+// //     console.log(user[key])
+// // }
+
+// // for(const key of user){
+// //     console.log([key])
+// // }
+
+
+// // let array =[1,2,3,4,5,6,7,8,9]
+// // for(const index in array){
+// //     console.log(array[index])
+
+// // }
+
+// // for(const value of array){
+// //     console.log(value)
+// // }
+
+
+
+
+// let myObjOne = {
+
+//     name: "Sydney",
+//     population: 25000,
+//     temp: 10,
+//     currency:    "$100",
 // }
 
-// for(const key in user){
-//     console.log(user[key])
+
+// let myObjTwo = {
+
+//     name: "OkayPop",
+//     population: 13000,
+//     temp: 10,
+//     currency:    "$20",
 // }
 
-// for(const key of user){
-//     console.log(user[key])
-// }
+// let myObjThree = {
 
-// for(const key of user){
-//     console.log([key])
-// }
-
-
-// let array =[1,2,3,4,5,6,7,8,9]
-// for(const index in array){
-//     console.log(array[index])
-
-// }
-
-// for(const value of array){
-//     console.log(value)
+//     name: "Moh",
+//     population: 290000,
+//     temp: 10,
+//     currency:    "$120",
 // }
 
 
+// let myObjFour = {
 
-
-let myObjOne = {
-
-    name: "Sydney",
-    population: 25000,
-    temp: 10,
-    currency:    "$100",
-}
-
-
-let myObjTwo = {
-
-    name: "OkayPop",
-    population: 13000,
-    temp: 10,
-    currency:    "$20",
-}
-
-let myObjThree = {
-
-    name: "Moh",
-    population: 290000,
-    temp: 10,
-    currency:    "$120",
-}
-
-
-let myObjFour = {
-
-    name: "Beejay",
-    population: 98000,
-    temp: 10,
-    currency:    "$850",
-}
-
-
-let myObjFive = {
-
-    name: "praise",
-    population: 72000,
-    temp: 10,
-    currency:    "$230",
-}
-
-
-let myObjSix = {
-
-    name: "Timi",
-    population: 30000,
-    temp: 10,
-    currency:    "$180",
-}
-
-
-
-let myObjSeven = {
-
-    name: "Izu",
-    population: 50000,
-    temp: 10,
-    currency:    "$600",
-}
-
-
-let myObjEight = {
-
-    name: "Dayo",
-    population: 15000,
-    temp: 10,
-    currency:    "$150",
-}
-
-
-let myObjNine = {
-
-    name: "Victor",
-    population: 35000,
-    temp: 10,
-    currency:    "$50",
-}
-
-
-// for(const key in ){
-//     console.log(user[key])
+//     name: "Beejay",
+//     population: 98000,
+//     temp: 10,
+//     currency:    "$850",
 // }
 
-let beforeMath= [[null,myObjOne,myObjTwo,myObjThree],[myObjFour,myObjFive,myObjSix,null],[myObjSeven,null,myObjEight,myObjNine]]
 
-let addedCalculation= 0
+// let myObjFive = {
 
-for(const value of beforeMath){
-    for(const index in value){
-        if(value[index] != null){
-            addedCalculation += value[index].population
-        }
-    }
-}
+//     name: "praise",
+//     population: 72000,
+//     temp: 10,
+//     currency:    "$230",
+// }
 
-console.log(addedCalculation)
+
+// let myObjSix = {
+
+//     name: "Timi",
+//     population: 30000,
+//     temp: 10,
+//     currency:    "$180",
+// }
+
+
+
+// let myObjSeven = {
+
+//     name: "Izu",
+//     population: 50000,
+//     temp: 10,
+//     currency:    "$600",
+// }
+
+
+// let myObjEight = {
+
+//     name: "Dayo",
+//     population: 15000,
+//     temp: 10,
+//     currency:    "$150",
+// }
+
+
+// let myObjNine = {
+
+//     name: "Victor",
+//     population: 35000,
+//     temp: 4,
+//     currency:    "$50",
+// }
+
+
+// // for(const key in ){
+// //     console.log(user[key])
+// // }
+
+// let beforeMath= [[null,myObjOne,myObjTwo,myObjThree],[myObjFour,myObjFive,myObjSix,null],[myObjSeven,null,myObjEight,myObjNine]]
+
+// let addedCalculation= 0
+
+// for(const value of beforeMath){
+//     for(const index in value){
+//         if((value[index] != null) && (value[index].temp < 10)){
+//             addedCalculation += value[index].population
+//          }
+//     }
+// }
+
+// console.log(addedCalculation)
+
+
